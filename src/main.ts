ea7c9014fc996db557609ce9b4388a868e2829e1
imranprof <imranpers@gmail.com>
2025-11-01 05:22:49 +0600

    add deploy origin to main

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
     app.enableCors({
-    origin: 'http://localhost:3001', // your Next.js frontend URL
+    origin: ['http://localhost:3001', 'https://task-manager-frontend-mu-red.vercel.app'], //for production 
+    // origin: true, //for test
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, // if you need cookies/auth
+    credentials: true, //for cokies auth
   });
   await app.listen(process.env.PORT ?? 3000);
 }
