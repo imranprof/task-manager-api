baecdddcf440ddc59388daf4451d8b4e8d79f3d3
imranprof <imranpers@gmail.com>
2025-11-01 03:57:49 +0600

    feat: add frontend origin for cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+    app.enableCors({
+    origin: 'http://localhost:3001', // your Next.js frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // if you need cookies/auth
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
