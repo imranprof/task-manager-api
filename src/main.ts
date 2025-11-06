59baee1714f1ae4b82ae0edb0405669b35545e55
imranprof <imranpers@gmail.com>
2025-11-06 17:58:11 +0600

    add origin true for cors error

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-    origin: ['http://localhost:3001', 'https://task-manager-frontend-mu-red.vercel.app'], //for production 
-    // origin: true, //for test
+    // origin: ['http://localhost:3001', 'https://task-manager-frontend-mu-red.vercel.app'], //for production 
+    origin: true, //for test
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true, //for cokies auth
   });
